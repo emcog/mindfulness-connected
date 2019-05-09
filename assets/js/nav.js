caa9af4ad29e923156975cfecfa2b231e72ff3dd
emCog <robin.leonard@gmail.com>
2019-05-09 14:39:21 +0100

    fixes to mobile nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -4,50 +4,29 @@
 const navContent = document.querySelector('.nav__mobile-contents');
 const navButton = document.querySelector('#nav__mobile-button');
 
-navButton.addEventListener('touchend', navButtonTouched, false);
-navButton.addEventListener('mouseup', navButtonClicked, false);
+navButton.addEventListener('click', navButtonPressed);
+// navButton.addEventListener('mouseup', navButtonPressed, false);
 
-function navButtonClicked(event) { 
+// ['mouseup', 'touchend'].forEach(evnt => navButton.addEventListener(evnt, navButtonPressed, false));
+
+
+function navButtonPressed(event) { 
 	if (navContent.classList.contains('nav__mobile-contents_is-active')) {
+    // stop a touch event becoming a mouse event, ie, stop the event sequence at this event
+    // event.preventDefault();
 	//colapse menu 
     navContent.classList.remove('nav__mobile-contents_is-active');
     navButton.classList.remove('nav__mobile-button_is-active')
     // console.log(event)
     // hide open tabs/expanded drop-down
     deactivateTabs()
-    // stop a touch event becoming a mouse event, ie, stop the event sequence at this event
-    // event.preventDefault();
   } else { 
-    console.log(event)
-  	//expand menu
-    navContent.classList.add('nav__mobile-contents_is-active');
-    navButton.classList.add('nav__mobile-button_is-active');
     // stop a touch event becoming a mouse event, ie, stop the event sequence at this event
     // event.preventDefault();
-  }
-}
-
-
-/* this WET is implemented because I couldn't get event.preventDefault to suppress the generation of further mouse click events. 
-the only fix I could find was to call a separate function for mouse events and touch events */
-
-function navButtonTouched(event) { 
-	if (navContent.classList.contains('nav__mobile-contents_is-active')) {
-	//colapse menu 
-    navContent.classList.remove('nav__mobile-contents_is-active');
-    navButton.classList.remove('nav__mobile-button_is-active')
     // console.log(event)
-    // hide open tabs/expanded drop-down
-    deactivateTabs()
-    // stop a touch event becoming a mouse event, ie, stop the event sequence at this event
-    // event.preventDefault();
-  } else { 
-    console.log(event)
   	//expand menu
     navContent.classList.add('nav__mobile-contents_is-active');
     navButton.classList.add('nav__mobile-button_is-active');
-    // stop a touch event becoming a mouse event, ie, stop the event sequence at this event
-    // event.preventDefault();
   }
 }
 
@@ -61,8 +40,8 @@ function navButtonTouched(event) {
 //navTab 
 let navTabs = document.querySelectorAll('.nav__mobile-contents > li');
 
-navTabs.forEach(navTab => navTab.addEventListener('touchend', navTabPressed));
-navTabs.forEach(navTab => navTab.addEventListener('mouseup', navTabPressed));
+navTabs.forEach(navTab => navTab.addEventListener('click', navTabPressed));
+// navTabs.forEach(navTab => navTab.addEventListener('mouseup', navTabPressed));
 
 
 function deactivateTabs() {
@@ -88,17 +67,6 @@ function navTabPressed() {
 		setTimeout(deactivateTabs(),1000);
 		// 02 activate this tab
 		this.classList.add('menu__tab_is-active');
-
-
-		/* todo the steps necessry if the dimms are not hard coded in*/
-		// 1 Hide all children of expanded tabs
-		// 2 Hide dropdown menu
-		// 3 Collapse all dropdown menus
-		// 4 Declare the active tabs to start the process getting its (expanded) dimensions
-		// 5 Show the active tabs children
-		// 6 Manipulate GUI
-		// 7 Get the dimensions (of 4)
-		// 8 Assign the dimensions
 	}
 
 }
